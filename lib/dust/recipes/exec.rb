b6e9d06041ffb607989ba814cdbfe3b105dc636e
kris kechagia <kk@rndsec.net>
2011-10-05 23:23:21 +0200

    implements exec recipe

---
+++ lib/dust/recipes/exec.rb
@@ -0,0 +1,26 @@
+class Deploy::Exec < Thor
+  namespace :exec
+
+  method_options :command => :string
+
+  desc "#{namespace}:cmd --command CMD", "runs command on all hosts"
+  def cmd
+    servers = invoke 'deploy:start'
+
+    exit unless options[:command]
+
+    servers.each do | server |
+      Dust.print_hostname server
+
+      print " - running command: #{options[:command]}"
+      ret = server.exec options[:command]
+      Dust.print_result ret[:exit_code]
+
+      puts ret[:stdout].chomp if ret[:stdout].length
+      puts ret[:stderr].chomp if ret[:stderr].length
+
+      server.disconnect
+    end
+  end
+end
+
