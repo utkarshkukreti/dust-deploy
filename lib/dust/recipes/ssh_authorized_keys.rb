b7b08c4761eee675ee9b89af4652232c32a4471a
kris kechagia <kk@rndsec.net>
2011-10-13 20:08:36 +0200

    ssh_authorized_keys: connects in runner now, not in recipe

--- lib/dust/recipes/ssh_authorized_keys.rb
+++ lib/dust/recipes/ssh_authorized_keys.rb
@@ -4,14 +4,10 @@ module Dust
 class Deploy
   private
   def ssh_authorized_keys node, ingredients
-    module_path = "./templates/#{ File.basename(__FILE__).chomp( File.extname(__FILE__) ) }"
+    template_path = "./templates/#{ File.basename(__FILE__).chomp( File.extname(__FILE__) ) }"
 
     # load users and their ssh keys from yaml file
-    users = YAML.load_file "#{module_path}/users.yaml"
-
-    # connect to server
-    server = Dust::Server.new node
-    server.connect
+    users = YAML.load_file "#{template_path}/users.yaml"
 
     authorized_keys = Hash.new
     ingredients.each do |remote_user, ssh_users|
@@ -33,38 +29,34 @@ class Deploy
 
       # check and create necessary directories
       print " - checking whether ~#{remote_user}/.ssh exists"
-      unless Dust.print_result server.exec("test -d ~#{remote_user}/.ssh")[:exit_code]
+      unless Dust.print_result node.exec("test -d ~#{remote_user}/.ssh")[:exit_code]
         print "   - creating ~#{remote_user}/.ssh"
-        unless Dust.print_result server.exec("mkdir ~#{remote_user}/.ssh")[:exit_code]
+        unless Dust.print_result node.exec("mkdir ~#{remote_user}/.ssh")[:exit_code]
           puts
           next
         end
       end
 
       # deploy authorized_keys
-      server.write "~#{remote_user}/.ssh/authorized_keys", authorized_keys
+      node.write "~#{remote_user}/.ssh/authorized_keys", authorized_keys
 
       # check permissions
-      server.chown "#{remote_user}:#{remote_user}", "~#{remote_user}/.ssh"
-      server.chmod '0644', "~#{remote_user}/.ssh/authorized_keys"
+      node.chown "#{remote_user}:#{remote_user}", "~#{remote_user}/.ssh"
+      node.chmod '0644', "~#{remote_user}/.ssh/authorized_keys"
 
 
       # remove authorized_keys files for all other users
       if options.cleanup?
         puts ' - deleting other authorized_keys files'
-        server.get_system_users(true).each do |user|
+        node.get_system_users(true).each do |user|
           next if users.keys.include? user
-          if server.file_exists? "~#{user}/.ssh/authorized_keys", true
+          if node.file_exists? "~#{user}/.ssh/authorized_keys", true
             print '  '
-            server.rm "~#{user}/.ssh/authorized_keys"
+            node.rm "~#{user}/.ssh/authorized_keys"
            end
         end
       end
-
-      puts
     end
-
-    server.disconnect
   end
 end
 end
