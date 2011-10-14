fe6a9b3c0db8ec05c5a55f24bf95f92ca372ea5c
kris kechagia <kk@rndsec.net>
2011-10-14 13:08:29 +0200

    migrates duplicity recipe

---
+++ lib/dust/recipes/duplicity.rb
@@ -0,0 +1,64 @@
+require 'erb'
+
+module Dust
+  class Deploy
+    private
+
+    # sets the system locale
+    def duplicity node, config
+      template_path = "./templates/#{ File.basename(__FILE__).chomp( File.extname(__FILE__) ) }"
+
+      # if the config simply sais "remove" then remove all duplicity cronjobs
+      return remove_duplicity_cronjobs node if config == 'remove'
+
+      # if hosts and directory config options are not given, use hostname of this node
+      config['hosts'] ||= [ node['hostname'] ]
+      config['directory'] ||= node['hostname']
+
+      # check whether backend is specified, skip to next scenario if not
+      return Dust.print_failed 'no backend specified.' unless config['backend']
+
+      # check if interval is correct   
+      unless [ 'monthly', 'weekly', 'daily', 'hourly' ].include? config['interval']
+        return Dust.print_failed "invalid interval: '#{config['interval']}'"
+      end
+
+      # clear all other duplicity cronjobs that might have been deployed earlier
+      remove_duplicity_cronjobs node 
+
+      # check whether we need ncftp
+      node.install_package 'ncftp' if config['backend'].include? 'ftp://'
+
+      # add hostkey to known_hosts
+      if config['backend'].include? 'scp://' and config['hostkey']
+        print '   - checking if ssh key is in known_hosts'
+        unless Dust.print_result node.exec("grep -q '#{config['hostkey']}' ~/.ssh/known_hosts")[:exit_code] == 0
+          print '   - adding host key to known_hosts'
+          Dust.print_result node.exec("mkdir ~/.ssh; echo '#{config['hostkey']}' >> ~/.ssh/known_hosts")[:exit_code]
+        end
+      end
+
+      # generate path for the cronjob
+      cronjob_path = "/etc/cron.#{config['interval']}/duplicity"
+      cronjob_path += "-#{config['title']}" if config['title']
+
+      # adjust and upload cronjob
+      template = ERB.new File.read("#{template_path}/cronjob.erb"), nil, '%<>'
+      print "   - adjusting and deploying cronjob (interval: #{config['interval']})"
+      node.write cronjob_path, template.result(binding), true
+      Dust.print_ok
+
+      # making cronjob executeable
+      node.chmod '0700', cronjob_path
+    end
+
+    # removes all duplicity cronjobs
+    def remove_duplicity_cronjobs node
+      print ' - deleting old duplicity cronjobs'
+      node.rm '/etc/cron.*/duplicity*', true
+      Dust.print_ok
+    end
+  end
+
+end
+
