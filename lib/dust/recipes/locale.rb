399d81e04bb73cb755599a1b63322ea9dbf55bf9
kris kechagia <kk@rndsec.net>
2011-10-14 16:12:23 +0200

    adds centos support for locale recipe

--- lib/dust/recipes/locale.rb
+++ lib/dust/recipes/locale.rb
@@ -4,8 +4,13 @@ module Dust
 
     # sets the system locale
     def locale node, locale
-      return unless node['os'] == 'debian' or node['os'] == 'ubuntu'
-      node.write '/etc/default/locale', "LANGUAGE=#{locale}\nLANG=#{locale}\nLC_ALL=#{locale}\n"
+      if node['os'] == 'debian' or node['os'] == 'ubuntu'
+        node.write '/etc/default/locale', "LANGUAGE=#{locale}\nLANG=#{locale}\nLC_ALL=#{locale}\n"
+      elsif node['os'] == 'centos'
+        node.write '/etc/sysconfig/i18n', "LANG=\"#{locale}\"\nLC_ALL=\"#{locale}\"\nSYSFONT=\"latarcyrheb-sun16\"\n"
+      else
+        Dust.print_failed 'os not supported'
+      end
     end
   end
 end
