baf22d152b96011b2a1bb238bbd7b87103a0c430
kris kechagia <kk@rndsec.net>
2011-10-14 23:44:06 +0200

    adds new colors

--- lib/dust/print_status.rb
+++ lib/dust/print_status.rb
@@ -1,10 +1,14 @@
 module Dust
   # colors for terminal
-  @red   = "\033[1;31m"
-  @blue   = "\033[1;34m"
-  @green  = "\033[1;32m"
-  @yellow = "\033[1;33m"
-  @none   = "\033[0m"
+  @red      = "\033[1;31m"
+  @green    = "\033[1;32m"
+  @yellow   = "\033[1;33m"
+  @blue     = "\033[1;34m"
+  @pink     = "\033[1;35m"
+  @turquois = "\033[1;36m"
+  @grey     = "\033[1;37m"
+  @black    = "\033[1;38m"
+  @none     = "\033[0m"
 
   $stdout.sync = true # autoflush
 
