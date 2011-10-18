e9f19899edc8406bb7133aff4529b73b5268558d
kris kechagia <kk@rndsec.net>
2011-10-18 20:54:03 +0200

    implements Dust.<color> methods, available from everywhere

--- lib/dust/print_status.rb
+++ lib/dust/print_status.rb
@@ -1,14 +1,14 @@
 module Dust
   # colors for terminal
-  @red      = "\033[1;31m"
-  @green    = "\033[1;32m"
-  @yellow   = "\033[1;33m"
-  @blue     = "\033[1;34m"
-  @pink     = "\033[1;35m"
-  @turquois = "\033[1;36m"
-  @grey     = "\033[1;37m"
-  @black    = "\033[1;38m"
-  @none     = "\033[0m"
+  def self.red;      "\033[1;31m"; end
+  def self.green;    "\033[1;32m"; end
+  def self.yellow;   "\033[1;33m"; end
+  def self.blue;     "\033[1;34m"; end
+  def self.pink;     "\033[1;35m"; end
+  def self.turquois; "\033[1;36m"; end
+  def self.grey;     "\033[1;37m"; end
+  def self.black;    "\033[1;38m"; end
+  def self.none;     "\033[0m"; end
 
   $stdout.sync = true # autoflush
 
@@ -23,23 +23,23 @@ module Dust
   end
 
   def self.print_ok string="", level=0
-    print_msg "#{string} #{@blue}[ ok ]#{@none}\n", level
+    print_msg "#{string} #{blue}[ ok ]#{none}\n", level
   end
 
   def self.print_failed string="", level=0
-    print_msg "#{string} #{@red}[ failed ]#{@none}\n", level
+    print_msg "#{string} #{red}[ failed ]#{none}\n", level
   end
 
   def self.print_warning string="", level=0
-    print_msg "#{string} #{@yellow}[ warning ]#{@none}\n", level
+    print_msg "#{string} #{yellow}[ warning ]#{none}\n", level
   end
 
   def self.print_hostname hostname, level=0
-    print_msg "\n[ #{@blue}#{hostname}#{@none} ]\n\n", level
+    print_msg "\n[ #{blue}#{hostname}#{none} ]\n\n", level
   end
 
   def self.print_recipe recipe, level=0
-    print_msg "#{@green}|#{recipe}|#{@none}\n", level
+    print_msg "#{green}|#{recipe}|#{none}\n", level
   end
 
   # indent according to level
