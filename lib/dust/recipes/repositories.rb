d3e8223b45a64cc3733dd78145865531fc3a89c9
kris kechagia <kk@rndsec.net>
2011-10-25 18:10:29 +0200

    implements sources.list generation for ubuntu as well

--- lib/dust/recipes/repositories.rb
+++ lib/dust/recipes/repositories.rb
@@ -16,20 +16,40 @@ module Dust
           repo = Hash.new unless repo.class == Hash
 
           # setting defaults
-          repo['url'] ||= 'http://ftp.debian.org/debian/'
+          repo['url'] ||= 'http://ftp.debian.org/debian/' if node.is_debian? true
+          repo['url'] ||= 'http://archive.ubuntu.com/ubuntu/' if node.is_ubuntu? true
+     
           repo['release'] ||= node['lsbdistcodename']
           repo['components'] ||= 'main'
 
-          # the default repository in /etc/apt/sources.list
+          # the default repository in /etc/apt/sources.list (debian)
           if name == 'default'
             Dust.print_msg 'deploying default repository'
-            Dust.print_result node.write('/etc/apt/sources.list',
-              "deb #{repo['url']} #{repo['release']} #{repo['components']}\n" +
-              "deb-src #{repo['url']} #{repo['release']} #{repo['components']}\n\n" +
-              "deb http://security.debian.org/ #{repo['release']}/updates #{repo['components']}\n" +
-              "deb-src http://security.debian.org/ #{repo['release']}/updates #{repo['components']}\n\n" +
-              "deb #{repo['url']} #{repo['release']}-updates #{repo['components']}\n" +
-              "deb-src #{repo['url']} #{repo['release']}-updates #{repo['components']}\n", true)
+
+            sources = String.new
+            sources += "deb #{repo['url']} #{repo['release']} #{repo['components']}\n" +
+                       "deb-src #{repo['url']} #{repo['release']} #{repo['components']}\n\n"
+
+            # security
+            if node.is_debian? true
+              sources += "deb http://security.debian.org/ #{repo['release']}/updates #{repo['components']}\n" +
+                         "deb-src http://security.debian.org/ #{repo['release']}/updates #{repo['components']}\n\n"
+            elsif node.is_ubuntu? true
+              sources += "deb #{repo['url']} #{repo['release']}-security #{repo['components']}\n" +
+                         "deb-src #{repo['url']} #{repo['release']}-security #{repo['components']}\n\n"
+            end
+
+            # updates
+            sources += "deb #{repo['url']} #{repo['release']}-updates #{repo['components']}\n" +
+                       "deb-src #{repo['url']} #{repo['release']}-updates #{repo['components']}\n\n"
+
+            # proposed
+            if node.is_ubuntu? true
+              sources += "deb #{repo['url']} #{repo['release']}-proposed #{repo['components']}\n" +
+                        "deb-src #{repo['url']} #{repo['release']}-proposed #{repo['components']}\n\n"
+            end
+
+            Dust.print_result node.write('/etc/apt/sources.list', sources, true)
             next
           end
 
