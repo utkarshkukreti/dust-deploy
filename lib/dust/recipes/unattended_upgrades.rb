929d66c47a8a9327f305a871269198c4c11b78bd
kris kechagia <kk@rndsec.net>
2011-10-27 13:22:07 +0200

    adds unattended_upgrades ubuntu support

--- lib/dust/recipes/unattended_upgrades.rb
+++ lib/dust/recipes/unattended_upgrades.rb
@@ -5,9 +5,9 @@ module Dust
     def unattended_upgrades node, ingredients
       template_path = "./templates/#{ File.basename(__FILE__).chomp( File.extname(__FILE__) ) }"
 
-      return unless node.is_debian?
+      return unless node.is_os? [ 'debian', 'ubuntu' ]
       node.install_package 'unattended-upgrades'
-      node.scp "#{template_path}/02periodic", '/etc/apt/apt.conf.d/02periodic'
+      node.scp "#{template_path}/periodic", '/etc/apt/apt.conf.d/02periodic'
     end
   end
 end
