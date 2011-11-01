5dbc4e1773bf3a340f799dae06ac51e7ed241bec
kris kechagia <kk@rndsec.net>
2011-11-01 11:45:06 +0100

    generate periodic file for unattended upgrades from config

--- lib/dust/recipes/unattended_upgrades.rb
+++ lib/dust/recipes/unattended_upgrades.rb
@@ -2,12 +2,30 @@ module Dust
   class Deploy
 
     private
-    def unattended_upgrades node, ingredients
+    def unattended_upgrades node, config
       template_path = "./templates/#{ File.basename(__FILE__).chomp( File.extname(__FILE__) ) }"
 
       return unless node.is_os? [ 'debian', 'ubuntu' ]
       node.install_package 'unattended-upgrades'
-      node.scp "#{template_path}/periodic", '/etc/apt/apt.conf.d/02periodic'
+
+      config = Hash.new unless config.class == Hash
+
+      # set defaults for non-set config
+      config['enable'] ||= 1
+      config['update-package-lists'] ||= 1
+      config['unattended-upgrade'] ||= 1
+      config['autocleaninterval'] ||= 1
+      config['verbose'] ||= 0
+
+      # generate configuration file
+      periodic = String.new
+      periodic += "APT::Periodic::Enable \"#{config['enable']}\";\n"
+      periodic += "APT::Periodic::Update-Package-Lists \"#{config['update-package-lists']}\";\n"
+      periodic += "APT::Periodic::Unattended-Upgrade \"#{config['unattended-upgrade']}\";\n"
+      periodic += "APT::Periodic::AutocleanInterval \"#{config['autocleaninterval']}\";\n"
+      periodic += "APT::Periodic::Verbose \"#{config['verbose']}\";\n"
+
+      node.write '/etc/apt/apt.conf.d/02periodic', periodic      
     end
   end
 end
