3cbcbe4f3a28ac054860a9cc125b6555a0edac20
kris kechagia <kk@rndsec.net>
2011-11-22 15:37:27 +0100

    locale recipe: switches from is_os to is_$packetmanager

--- lib/dust/recipes/locale.rb
+++ lib/dust/recipes/locale.rb
@@ -4,11 +4,11 @@ module Dust
 
     # sets the system locale
     def locale node, locale
-      if node.is_os? ['debian', 'ubuntu'], true
+      if node.uses_apt? true
         Dust.print_msg "setting locale to '#{locale}'"
         node.write '/etc/default/locale', "LANGUAGE=#{locale}\nLANG=#{locale}\nLC_ALL=#{locale}\nLC_CTYPE=#{locale}\n", true
         Dust.print_ok
-      elsif node.is_os? ['scientific', 'redhat', 'centos'], true
+      elsif node.uses_rpm? true
         Dust.print_msg "setting locale to '#{locale}'"
         node.write '/etc/sysconfig/i18n', "LANG=\"#{locale}\"\nLC_ALL=\"#{locale}\"\nSYSFONT=\"latarcyrheb-sun16\"\n", true
         Dust.print_ok
